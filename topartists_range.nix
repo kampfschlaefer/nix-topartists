1850b7c0daab4fe6f88af8d8a104238ef19ad4b0
Arnold Krille <arnold@arnoldarts.de>
2015-11-18 17:13:46 +0100

    Start with top artists over several days

---
+++ topartists_range.nix
@@ -0,0 +1,27 @@
+{ stdenv, pkgs, range }:
+
+let
+  startday = builtins.elemAt range 0;
+  endday = builtins.elemAt range 1;
+  days = builtins.genList (x: (if x < 9 then "0" else "") + builtins.toString (x + startday)) (endday - startday + 1);
+  daysbuilds = map (x: pkgs.callPackage ./dailyplays.nix { day="2015-10-${x}"; }) days;
+  daysinputs = pkgs.lib.concatMapStrings(x: "${x}/dailyplays.ldj ") daysbuilds;
+
+in stdenv.mkDerivation rec {
+  name = "topartists-${builtins.toString startday}_${builtins.toString endday}";
+
+  src = ./scripts;
+
+  buildInputs = [
+    pkgs.python34
+  ] ++ daysbuilds;
+
+  buildPhase = "
+    python3 topartists.py ${daysinputs}
+  ";
+
+  installPhase = "
+    mkdir $out
+    cp topartists.ldj $out/
+  ";
+}
